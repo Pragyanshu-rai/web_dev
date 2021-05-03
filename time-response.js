545fd0f95f8b04f0994a8d81a7a49f7cb402a74e
Pragyanshu-rai <pragyanshur@gmail.com>
2021-05-04 02:33:51 +0530

    webdev

---
+++ time-response.js
@@ -0,0 +1,10 @@
+var today = new Date()
+
+function show()
+{
+    week=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    document.getElementById("day").innerHTML = week[today.getDay()];
+    document.getElementById("date").innerHTML = today.gettoday();
+    document.getElementById("hr").innerHTML = today.getHours();
+    document.getElementById("min").innerHTML = today.getMinutes();
+}
