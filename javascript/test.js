dd2f40bbe4ad2fb9eb46a28e885190e05e625ac3
Pragyanshu-rai <pragyanshur@gmail.com>
2021-05-24 11:46:55 +0530

    web-dev

--- javascript/test.js
+++ javascript/test.js
@@ -1,3 +1,8 @@
+function start() {
+  first();
+  second();
+}
+
 function first() {
   var paras = document.getElementsByTagName("p");
   console.log(paras.length);
@@ -6,12 +11,24 @@ function first() {
   for (let i = 0; i < paras.length; i++) {
     var data = paras[i].innerText;
     // document.write(i+" "+data+"<br />");
-    console.log(i+" "+data);
+    console.log(i + " " + data);
   }
   var dodgers = document.getElementsByClassName("dodger");
   for (let index = 0; index < dodgers.length; index++) {
-      // dodgers[index].innerHTML += " - dodger";
-      dodgers[index].style.color = "dodgerblue";  
+    // dodgers[index].innerHTML += " - dodger";
+    dodgers[index].style.color = "dodgerblue";
   }
+}
+
+function second() {
   console.log(document.querySelectorAll(".dodger, .red"));
+  var h2 = document.createElement("h2");
+  var txt = document.createTextNode("Appended using javascript");
+  var cmt = document.createComment("This h2 tag and text is added using javascript!");
+  // h2.innerHTML = "Appended using javascript";
+  h2.appendChild(txt);
+  var pnode = document.getElementsByClassName("testing")[0];
+  pnode.appendChild(cmt);
+  pnode.appendChild(h2);
+  console.log("h2 tag appended");
 }
